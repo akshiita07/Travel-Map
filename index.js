6de4c90a48a14d64c5619c8bcc86df28ee2cf781
Akshita Pathak <pathakshita07@gmail.com>
2024-06-17 00:06:57 +0530

    Update index.js
    Added post request

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-﻿//npm init
+//npm init
 //npm i express body-parser ejs pg
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -25,33 +25,80 @@ const db = new pg.Client({
 
 db.connect();   //start connection to db
 
-db.query("SELECT code FROM visited_countries",function(err,res){
-    if(err){
-        console.error("Some error occurred: ",err.stack);
-    }else{
-        // console.log(res.rows);      //returns all rows in visited_countries table
-
-        //countrydb = res.rows;     if this method then we have to JSON stringify n parse
-        for(var i=0;i<res.rows.length;i++){
-            countrydb.push(res.rows[i].code);
+app.get('/', async (req, res) => {
+    const result = await db.query("SELECT code FROM visited_countries")
+    try {
+        for (var i = 0; i < result.rows.length; i++) {
+            countrydb.push(result.rows[i].code);
         }
+        // console.log(result.rowCount);
 
-        console.log(countrydb);
+
+        console.log("The countries queried are: "+countrydb);
+        // console.log(countrydb); array:[ { id: 1, code: 'FR' }, { id: 2, code: 'GB' }, { id: 3, code: 'US' } ]
+        res.render('index.ejs', {
+            // converts JavaScript objects (here array) into JSON string
+            // console.log(JSON.stringify(countrydb));  [{"id":1,"code":"FR"},{"id":2,"code":"GB"},{"id":3,"code":"US"}]
+            countryOutput: countrydb,
+            totalCountriesSelected: result.rowCount,
+        });
+    } catch (err) {
+        console.error("Some error occurred: ", err.stack);
     }
+
+
     //close connection:
-    db.end();
+    // db.end();
 });
 
+app.post("/submit", async (req, res) => {
+    // console.log(req.body);  output:{ userInputCountry: 'spain' }
+    // console.log(req.body.userInputCountry);         op:spain
 
-app.get('/', (req, res) => {
-    // console.log(countrydb); array:[ { id: 1, code: 'FR' }, { id: 2, code: 'GB' }, { id: 3, code: 'US' } ]
-    res.render('index.ejs', {
-        // converts JavaScript objects (here array) into JSON string
-        // console.log(JSON.stringify(countrydb));  [{"id":1,"code":"FR"},{"id":2,"code":"GB"},{"id":3,"code":"US"}]
-        countryOutput: countrydb,
-        totalCountriesSelected: countrydb.length,
-    });
+    //convert inputs first letter to upper case & rest body to lowercase
+    // console.log(req.body.userInputCountry.charAt(0).toUpperCase());
+    // console.log(req.body.userInputCountry.slice(1).toLowerCase());
 
+    //first split input where space occurs-- ie north_korea
+    let userWords = req.body.userInputCountry.split(' ');
+    // console.log(userWords); [ 'South', 'Korea' ] saves words in array
+
+    let userInput = "";
+
+    // let userFirstChar = req.body.userInputCountry.charAt(0).toUpperCase();
+    // let userRemainChar = req.body.userInputCountry.slice(1).toLowerCase();
+    // let userInput = userFirstChar.concat(userRemainChar);
+    // console.log(userInput);
+
+    for (var i = 0; i < userWords.length; i++) {
+        userInput = userInput + userWords[i].charAt(0).toUpperCase() + userWords[i].slice(1).toLowerCase();
+        if (i < userWords.length - 1) {
+            userInput = userInput + " ";
+        }
+    }
+
+    console.log(userInput);
+
+    //fetch country code corresponding to country name:
+    const resultCode = await db.query("SELECT code FROM countries WHERE cname=$1", [userInput]);
+
+    if (resultCode.rowCount > 0) {
+        console.log(resultCode.rows);        //output:[ { code: 'ES' } ]
+        // console.log(resultCode.rows[0].code);   //as only 1 row is returned so index [0]
+
+        const codeToInsert = resultCode.rows[0].code;
+        // console.log(codeToInsert);
+
+        //add this code to visited_countries table:
+        const result = await db.query("INSERT INTO visited_countries(code) VALUES ($1)", [codeToInsert]);
+    }
+    else {
+        console.log("No country found!")
+    }
+
+    res.redirect('/');
+
+    // db.end();
 });
 
 app.listen(port, () => {
